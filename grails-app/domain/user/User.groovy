3408bbc37782f19f604c6458db2efdb0fe8de9de
Alexandar Vysotskiy <alexandar.vysotskiy@gmail.com>
2021-06-01 11:23:04 +0300

    (LESSON-8) DONE

--- grails-app/domain/user/User.groovy
+++
@@ -1,7 +0,0 @@
-package user
-
-class User {
-
-    static constraints = {
-    }
-}
--- grails-app/jobs/UserDataJob.groovy
+++ grails-app/jobs/UserDataJob.groovy
@@ -1,10 +1,25 @@
+import eighth.NewUser
+
 class UserDataJob {
 
+    def mailService
+
     static triggers = {
+//        simple repeatInterval: 1D // execute job once in 5 seconds
         simple repeatInterval: 5000l // execute job once in 5 seconds
     }
 
     def execute() {
-       println('Job works!!!!!!!!!!!!!')
+        println('Job works!!!')
+
+        sendUserInfo(NewUser.findAll())
+    }
+
+    def sendUserInfo(List<NewUser> list) {
+        mailService.sendMail {
+            to "bestof@list.ru"
+            subject "User data"
+            body "Right now in the app ${list.size()} users"
+        }
     }
 }
